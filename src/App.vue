f19b5598a8ebdb78a9df7e13987e41618e2e30a8
stasithyst <pechenkinaanastasiia2003@gmail.com>
2026-03-03 14:20:48 +0300

    add table

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import CoolTable from "@/components/coolTable.vue";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <cool-table />
+    </div>
+  </header>
+</template>
